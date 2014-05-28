304b6f7ab5da7e978457d459dd9a505dd9aa0ffd
GS888 <gsnydermail@gmail.com>
2014-05-28 18:28:40 -0400

    map mapping data. Doesnt yet have info square, or link to data page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,12 @@ class WelcomeController < ApplicationController
 
   def index
     @trees = Tree.all
+    @hash = Gmaps4rails.build_markers(@trees) do |tree, marker|
+      marker.lat tree.latitude
+      marker.lng tree.longitude
+    end
+  #@json = hash.to_json
+
   end
 
   def create
@@ -10,11 +16,8 @@ class WelcomeController < ApplicationController
 
 
 
-  @trees = Tree.all
-    @hash = Gmaps4rails.build_markers(@trees) do |tree, marker|
-      marker.lat tree.latitude
-      marker.lng tree.longitude
-    end
+
+
 
 
 
