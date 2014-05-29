43023d51145643562ffd0a69e9a4f29e63af1e47
GS888 <gsnydermail@gmail.com>
2014-05-29 12:41:05 -0400

    changed serve_static_assest to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,17 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # Disable Rails's static asset server (Apache or nginx will already do this). NO
+  config.serve_static_assets = true
+
+
+
+
+
+
+
+
+
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -80,4 +89,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+
 end
